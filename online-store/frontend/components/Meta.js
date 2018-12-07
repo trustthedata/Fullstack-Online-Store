78e08bd3fbd934a094bdf62b6f63a15b484dc855
Herbert Hodgson <herberthodgson@hotmail.com>
2018-12-06 22:47:30 -0500

    Started setting up Next.js routing, custom <App>, & styling

---
+++ online-store/frontend/components/Meta.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Meta = () => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <link rel="shortcut icon" href="/static/favicon(1).png" />
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+      <title>Lincoln Ave Luxury Linen</title>
+    </Head>
+  );
+};
+
+export default Meta;
